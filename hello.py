117fd3e2c5e5c56d7f30ad963ee45a73f749ece5
Branden Torrance <btorrance86@gmail.com>
2022-04-22 19:07:35 -0400

    Add variable and heading comment

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello World")
+"""
+Author: Branden Torrance
+
+This simple program is used to demonstrate the integration of
+Python, Visual Studio Code, and GitHub
+"""
+
+# Variable to display
+intro = "\nHello World!"
+
+# Display message
+print(intro)
